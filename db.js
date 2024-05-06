54a9a361e1b3769c4c07631dda60c70301bdc31f
Brady McAtee <bradymcatee12@gmail.com>
2024-05-05 17:03:43 -0700

    added ssl option on db.js

--- db.js
+++ db.js
@@ -10,6 +10,9 @@ const devConfig = {
 
 const proConfig = {
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 };
 const pool = new Pool(
   process.env.NODE_ENV === "production" ? proConfig : devConfig
